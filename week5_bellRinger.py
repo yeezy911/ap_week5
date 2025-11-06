7a69db830fcaad6275e81481e7ba46d6d779474b
yeezy911 <rmoralez4901@gmail.com>
2025-11-06 19:16:59 +0000

    problem set 4

--- week5_bellRinger.py
+++ week5_bellRinger.py
@@ -84,9 +84,13 @@ print(joined_motto_split)
 # a. Replace "busy" with "distracted".
 # b. Replace "plans" with "mistakes".
 
+sentence = "Life is what happens when you are busy making other plans."
+replaced_sentence = print(sentence.replace("busy", "distracted").replace("plans", "mistakes"))
+
 # Problem Set 4: String Properties and Advanced Operations
 # Repetition:
 # Concatenate the word "Iteration" 7 times.
+repeated_word = print("Iteration " * 7)
 
 # Word Search:
 # Check if the word "moonlight" appears in the quote: "With freedom, books, flowers, and the moon, who could not be happy? - Oscar Wilde"
